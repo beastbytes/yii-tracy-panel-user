27c464ae45073d054c68067ea9efb52ebe69251b
Chris <chris@clyates.plus.com>
2025-07-02 18:02:00 +0100

    Update config

--- config/params-web.php
+++ config/params-web.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'beastbytes/yii-tracy' => [
-        'panels' => [
+        'panelConfig' => [
             'user' => [
                 'class' => BeastBytes\Yii\Tracy\Panel\User\Panel::class,
             ],
